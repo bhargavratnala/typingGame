a513725084743ab71e9bfe55c8690afdd5d19edd
DeepikaBadiganti <deepikabadiganti@gmail.com>
2023-12-19 12:11:17 +0530

    mobile not supported

--- assets/script.js
+++ assets/script.js
@@ -116,8 +116,20 @@ function moveEnimies() {
 
 var intervel;
 
-setTimeout(() => {
-    gametext.classList.add("closetext");
-    setEnimies();
-    intervel = setInterval(moveEnimies, 100);
-}, 3000);
+
+if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)){
+            gametext.innerHTML = "not supported on mobile";
+}
+else{
+    setTimeout(() => {
+        gametext.classList.add("closetext");
+        setEnimies();
+        intervel = setInterval(moveEnimies, 100);
+    }, 3000);
+}
